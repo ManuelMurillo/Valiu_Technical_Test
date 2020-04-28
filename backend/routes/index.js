aa41aa3a9d93d8321c0d9ecf84128f438ea37235
ManuelMurillo <jmmurilloy@correo.udistrital.edu.co>
2020-04-28 15:35:06 -0500

    ajustes para socket.io con routing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-module.exports = router;
+module.exports = function(io) {
+
+  io.on("connection", function(socket) {
+    socket.emit("Tabs", []);
+    socket.on("Tabs", data => {
+      console.log(socket.id + " : " + JSON.stringify(data));
+      socket.broadcast.emit("Tabs", data);
+    });
+  });
+
+  return router;
+};
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const socket = io.connect("localhost:3000");
+const socket = io.connect("localhost:3000/");
 
 export default socket;
